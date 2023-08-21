051a29d671e7de1f8d6bb67b92d6b3edc5f1c4ff
abincaps <abincaps@qq.com>
2023-08-21 22:13:59 +0800

    修改启动日志

--- member/src/main/java/com/abincaps/train/member/config/MemberApplication.java
+++ member/src/main/java/com/abincaps/train/member/config/MemberApplication.java
@@ -1,14 +1,24 @@
 package com.abincaps.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.abincaps")
 public class MemberApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
-	}
 
+	private static final Logger logger = LoggerFactory.getLogger(MemberApplication.class);
+	public static void main(String[] args) {
+
+		SpringApplication app = new SpringApplication(MemberApplication.class);
+		Environment env = app.run(args).getEnvironment();
+
+		logger.info("启动成功!");
+		logger.info("地址：http://127.0.0.1:{}/hello", env.getProperty("server.port"));
+
+	}
 }
